a37b09883ca552c1588a3775d3b4441785400a70
Zorotic <swftapocalypse1@gmail.com>
2021-02-25 14:09:35 -0500

    Update Help.ts

--- src/commands/Help.ts
+++ src/commands/Help.ts
@@ -1,67 +1,68 @@
-import { Command } from "discord-akairo";
-import { Collection, Message, MessageEmbed } from "discord.js";
+import { Command } from 'discord-akairo';
+import { Collection, Message, MessageEmbed } from 'discord.js';
 
 export default class PingCommand extends Command {
-    public constructor() {
-        super("help", {
-            aliases: ["help", "commands"]
-        });
-    }
+	public constructor() {
+		super('help', {
+			aliases: ['help', 'commands'],
+		});
+	}
 
-    public exec(message: Message) {
-        const handler = this.handler;
+	public async exec(message: Message) {
+		const handler = this.handler;
 
-        let description: string = "Below are all commands listed which are available to use. The default prefix is: ";
+		let description: string = 'Below are all commands listed which are available to use. The default prefix is: ';
 
-        const prefix: string | Array<string> | any = handler.prefix;
+		const prefix: string | Array<string> | any = handler.prefix;
 
-        /**
-         * Check whether the prefix is an array of strings or a string or a PrefixSupplier
-         * If It is an array -> Get the first value
-         * If it is a string -> Just append the prefix to the description
-         * If it is a PrefixSupplier -> Use a default prefix (!)
-         */
-        if (Array.isArray(prefix))                // Array
-            description += `\`${prefix[0]}\``;
-        else if (typeof prefix == "string")       // String
-            description += `\`${prefix}\``;
-        else                                      // PrefixSupplier
-            description += `\`!\``;
+		/**
+		 * Check whether the prefix is an array of strings or a string or a PrefixSupplier
+		 * If It is an array -> Get the first value
+		 * If it is a string -> Just append the prefix to the description
+		 * If it is a PrefixSupplier -> Use a default prefix (!)
+		 */
+		if (Array.isArray(prefix)) {
+			description += `\`${prefix[0]}\``;
+		} else if (typeof prefix == 'string') {
+			description += `\`${prefix}\``;
+		} else {
+			description += '`!`';
+		}
 
 
-        const helpEmbed: MessageEmbed = new MessageEmbed()
-            .setTitle("Commands from " + this.client.user.username)
-            .setDescription("Below are all commands listed which are available to use. The default prefix is: ");
+		const helpEmbed: MessageEmbed = new MessageEmbed()
+			.setTitle('Commands from ' + this.client.user.username)
+			.setDescription('Below are all commands listed which are available to use. The default prefix is: ');
 
-        const commandsWithDescription: Collection<string, Array<Command>> = new Collection<string, Array<Command>>();
-        
-        /**
-         * Check whether the category is:
-         *  - Inside the Collection (commandsWithDescription) -> Push just the command into the collection entry
-         *  - Not inside the Collection (commandsWithDescription) -> Create a new entry with the category id into the collection and set the command as an array as value
-         */
-        handler.modules.map((v: Command) => {
-            const cat = v.category.filter((v: Command, k: string) => v.category.id == v.id && k != "default");
-            if (!commandsWithDescription.has(v.category.firstKey())) {
-                commandsWithDescription.set(cat.size > 1 ? cat.keyArray()[cat.size - 1] : cat.firstKey(), [v]);
-            } else {
-                const categoryID = cat.size > 1 ? cat.keyArray()[cat.size - 1] : cat.firstKey();
-                const arr = commandsWithDescription.get(categoryID);
-                arr.push(v);
-                commandsWithDescription.set(categoryID, arr);
-            }
-        });
+		const commandsWithDescription: Collection<string, Array<Command>> = new Collection<string, Array<Command>>();
 
-        /**
-         * Loop through each entry from commandsWithDescription and add each category as a field into the embed (helpEmbed)
-         */
-        commandsWithDescription.forEach((cat, k: string) => {
-            helpEmbed.addField(k, cat.map((v) => `\`${v.id}\``).join(", "), false);
-        });
+		/**
+		 * Check whether the category is:
+		 *  - Inside the Collection (commandsWithDescription) -> Push just the command into the collection entry
+		 *  - Not inside the Collection (commandsWithDescription) -> Create a new entry with the category id into the collection and set the command as an array as value
+		 */
+		handler.modules.map((v: Command) => {
+			const cat = v.category.filter((v: Command, k: string) => v.category.id == v.id && k != 'default');
+			if (!commandsWithDescription.has(v.category.firstKey())) {
+				commandsWithDescription.set(cat.size > 1 ? cat.keyArray()[cat.size - 1] : cat.firstKey(), [v]);
+			} else {
+				const categoryID = cat.size > 1 ? cat.keyArray()[cat.size - 1] : cat.firstKey();
+				const arr = commandsWithDescription.get(categoryID);
+				arr.push(v);
+				commandsWithDescription.set(categoryID, arr);
+			}
+		});
 
-        // Send the embed with a reply to the executing message
-        return message.util.send({ embed: helpEmbed, replyTo: message.id });
-    }
+		/**
+		 * Loop through each entry from commandsWithDescription and add each category as a field into the embed (helpEmbed)
+		 */
+		commandsWithDescription.forEach((cat, k: string) => {
+			helpEmbed.addField(k, cat.map((v) => `\`${v.id}\``).join(', '), false);
+		});
+
+		// Send the embed with a reply to the executing message
+		return message.util.send({ embed: helpEmbed, replyTo: message.id });
+	}
 }
 
 
